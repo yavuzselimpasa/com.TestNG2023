ef2c0d28d78459200e1b85af726c9369c65b9e6e
yavuzselimpasa <yavuzselimpasa.2019@gmail.com>
2023-05-24 21:24:01 +0300

    v-3

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -11,7 +11,15 @@ import java.time.Duration;
 
 public class Driver {
 
-    static WebDriver driver;
+    private static WebDriver driver;
+    private Driver(){}
+    /*
+    --> Singleton pattern kullanilarak driver objesine erisilmesini engelledik
+
+    --> Constructor'i private yaparak bu class'dan obje olusturularak
+           class uyelerinin kullanilmasinin onune gectik
+
+         */
     public static WebDriver getDriver(){
         String istenenBrowser = ConfigReader.getProperty("browser");
         // chrome, firefox, safari, edge
